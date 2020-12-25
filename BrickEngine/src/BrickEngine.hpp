a699798615c81ecc83d8b66a5faf9733223260b7
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2020-12-25 19:36:14 +1300

    Added Base.hpp

---
+++ BrickEngine/src/BrickEngine.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+// Precompiled Header
+#include "brickpch.hpp"
+
+// Core
+#include "BrickEngine/Core/Base.hpp"
---
+++ BrickEngine/src/BrickEngine/Core/Base.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "brickpch.hpp"
+
+namespace BrickEngine {
+
+	typedef unsigned char			uint8;
+	typedef unsigned short			uint16;
+	typedef unsigned int			uint32;
+	typedef unsigned long long		uint64;
+
+	typedef signed char				int8;
+	typedef signed short			int16;
+	typedef signed int				int32;
+	typedef signed long long		int64;
+
+	typedef float					float32;
+	typedef double					float64;
+
+	static_assert(sizeof(uint8) == 1, "Expected sizeof uint8 to be 1 byte.");
+	static_assert(sizeof(uint16) == 2, "Expected sizeof uint16 to be 2 bytes.");
+	static_assert(sizeof(uint32) == 4, "Expected sizeof uint32 to be 4 bytes.");
+	static_assert(sizeof(uint64) == 8, "Expected sizeof uint64 to be 8 bytes.");
+
+	static_assert(sizeof(int8) == 1, "Expected sizeof int8 to be 1 byte.");
+	static_assert(sizeof(int16) == 2, "Expected sizeof int16 to be 2 bytes.");
+	static_assert(sizeof(int32) == 4, "Expected sizeof int32 to be 4 bytes.");
+	static_assert(sizeof(int64) == 8, "Expected sizeof int64 to be 8 bytes.");
+
+	static_assert(sizeof(float32) == 4, "Expected sizeof float32 to be 4 bytes.");
+	static_assert(sizeof(float64) == 8, "Expected sizeof float64 to be 8 bytes.");
+
+}
+
+#if defined(_WIN32) || defined(_WIN64)
+	#define BRICKENGINE_PLATFORM_WINDOWS 1
+#elif defined(__linux__)
+	#define BRICKENGINE_PLATFORM_LINUX 1
+#elif defined(__APPLE__)
+	#define BRICKENGINE_PLATFORM_MAC 1
+#endif
+
+#define BRICKENGINE_ENABLE_ASSERTS 1
+#if BRICKENGINE_ENABLE_ASSERTS
+	#if defined(_MSC_VER)
+		#include <intrin.h>
+		#define BRICKENGINE_DEBUG_BREAK __debugbreak()
+	#else
+		#define BRICKENGINE_DEBUG_BREAK
+	#endif
+	#define BRICKENGINE_ASSERT(x) if (!(x)) { BRICKENGINE_DEBUG_BREAK; }
+#else
+	#define BRICKENGINE_ASSERT(x)
+#endif
